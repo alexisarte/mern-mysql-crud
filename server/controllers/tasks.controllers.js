aa87d7f4d4bfe78ee8f7821df7dc66ce529f3488
alexis <alexisrodriguezarteaga@gmail.com>
2022-11-19 20:55:36 -0300

    Update tasks endpoint

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,12 +1,16 @@
 import { pool } from '../db.js';
 
 export const getTasks = async (req, res) => {
-  const [result] = await pool.query('SELECT * FROM tasks ORDER BY createAt ASC')
+  const [result] = await pool.query(
+    'SELECT * FROM tasks ORDER BY createAt ASC'
+  );
   res.json(result);
 };
 
 export const getTask = async (req, res) => {
-  const [result] = await pool.query('SELECT * FROM tasks WHERE id = ?', [req.params.id]);
+  const [result] = await pool.query('SELECT * FROM tasks WHERE id = ?', [
+    req.params.id,
+  ]);
   if (result.length === 0)
     return res.status(404).json({ message: 'Task not found' });
   return res.json(result[0]);
@@ -26,12 +30,19 @@ export const createTask = async (req, res) => {
   });
 };
 
-export const updateTask = (req, res) => {
-  res.send('actualizando tarea');
+export const updateTask = async (req, res) => {
+  const { title, description } = req.body;
+  const result = await pool.query('UPDATE tasks SET ? WHERE id = ?', [
+    req.body,
+    req.params.id,
+  ]);
+  res.json(result);
 };
 
 export const deleteTask = async (req, res) => {
-  const [result] = await pool.query('DELETE FROM tasks WHERE id = ?', [req.params.id]);
+  const [result] = await pool.query('DELETE FROM tasks WHERE id = ?', [
+    req.params.id,
+  ]);
   if (result.affectedRows === 0)
     return res.status(404).json({ message: 'Task not found' });
   return res.sendStatus(204);
