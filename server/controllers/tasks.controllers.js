acf508c6232c5987621c6cdd4bc345827e98db17
alexis <alexisrodriguezarteaga@gmail.com>
2022-11-19 19:22:02 -0300

    Create tasks endpoint

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,13 +1,25 @@
+import { pool } from '../db.js';
+
 export const getTasks = (req, res) => {
   res.send('obteniendo tareas');
-}
+};
 
 export const getTask = (req, res) => {
   res.send('obteniendo una tarea');
 };
 
-export const createTask = (req, res) => {
-  res.send('creando tareas');
+export const createTask = async (req, res) => {
+  const { title, description } = req.body;
+  const [result] = await pool.query(
+    'INSERT INTO tasks(title, description) VALUES (?,?)',
+    [title, description]
+  );
+  console.log(result);
+  res.json({
+    id: result.insertId,
+    title,
+    description,
+  });
 };
 
 export const updateTask = (req, res) => {
