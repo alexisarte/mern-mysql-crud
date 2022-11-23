2bbdac00f174683d63b41f40ffdc1b84b3ca4b01
alexis <alexisrodriguezarteaga@gmail.com>
2022-11-23 18:14:04 -0300

    List Tasks Frontend

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react';
+import { getTasksRequest } from '../api/tasks.api';
+import TaskCard from '../components/TaskCard';
+
 function TasksPage() {
-  return <div>TasksPage</div>;
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function loadTasks() {
+      const response = await getTasksRequest();
+      setTasks(response.data);
+    }
+    loadTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      {tasks.map(task => (
+        <TaskCard task={task} key={task.id} />
+      ))}
+    </div>
+  );
 }
 
 export default TasksPage;
