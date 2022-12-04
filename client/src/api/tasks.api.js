091e7f8fc5b7887e9d3afb24f511b98d4bc0102c
alexis <alexisrodriguezarteaga@gmail.com>
2022-12-04 11:25:25 -0300

    Fix PORT

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
-import { DB_HOST, PORT } from '../../../server/config.js';
 
 export const getTasksRequest = async () =>
-  await axios.get(`http://${DB_HOST}:${PORT}/tasks`);
+  await axios.get('http://localhost:6347/tasks');
 
 export const createTaskRequest = async (task) =>
-  await axios.post(`http://${DB_HOST}:${PORT}/tasks`, task);
+  await axios.post('http://localhost:6347/tasks', task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`http://${DB_HOST}:${PORT}/tasks/${id}`);
+  await axios.delete(`http://localhost:6347/tasks/${id}`);
 
 export const getTaskRequest = async (id) =>
-  await axios.get(`http://${DB_HOST}:${PORT}/tasks/${id}`);
+  await axios.get(`http://localhost:6347/tasks/${id}`);
 
 export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`http://${DB_HOST}:${PORT}/tasks/${id}`, newFields);
+  await axios.put(`http://localhost:6347/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`http://${DB_HOST}:${PORT}/tasks/${id}`, { done });
+  await axios.put(`http://localhost:6347/tasks/${id}`, { done });
