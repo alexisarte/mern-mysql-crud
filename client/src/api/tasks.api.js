24b6c22aa2303e2a3c8f6272d3319e28c322d8eb
alexis <alexisrodriguezarteaga@gmail.com>
2022-12-04 11:33:36 -0300

    Fix Host

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 export const getTasksRequest = async () =>
-  await axios.get('http://localhost:6347/tasks');
+  await axios.get('http://containers-us-west-70.railway.app:6347/tasks');
 
 export const createTaskRequest = async (task) =>
-  await axios.post('http://localhost:6347/tasks', task);
+  await axios.post('http://containers-us-west-70.railway.app:6347/tasks', task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`http://localhost:6347/tasks/${id}`);
+  await axios.delete(`http://containers-us-west-70.railway.app:6347/tasks/${id}`);
 
 export const getTaskRequest = async (id) =>
-  await axios.get(`http://localhost:6347/tasks/${id}`);
+  await axios.get(`http://containers-us-west-70.railway.app:6347/tasks/${id}`);
 
 export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`http://localhost:6347/tasks/${id}`, newFields);
+  await axios.put(`http://containers-us-west-70.railway.app:6347/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`http://localhost:6347/tasks/${id}`, { done });
+  await axios.put(`http://containers-us-west-70.railway.app:6347/tasks/${id}`, { done });
